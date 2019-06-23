bb8bd69e1000d54a2f34953bd867965d4f66a188
AdhamJaber <adhamjaber@gmail.com>
2019-06-23 03:40:44 +0300

    modified file - adding comments

--- find_files.sh
+++ find_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-dir=$1
-string=$2
+dir=$1		  # first argument for path (directory)
+string=$2     # word to be searched
 
 
 function validate {
@@ -11,21 +11,21 @@ function validate {
 }
 
 
-if [ $# -eq 2 ]
+if [ $# -eq 2 ]   #check number of arguments sent
 then
-	if [ -d $1 ]
+	if [ -d $1 ]   # if argument 1 is a valid directory
          then
 	   if [[ -n $(find $1 -type f -print | xargs grep -l $2) ]]
 		then
-			find $1 -type f -print | xargs grep -l $2
+			find $1 -type f -print | xargs grep -l $2  # list all files contain the searched word
 		else
-			echo  "$2 isn't found .."
+			echo  "$2 isn't found .."   #word isn't found in given directory
 		fi
 		
 	else
 	   echo "$1 is not a valid directory !! please try again .."
 	fi
 else
-validate
+validate   #call the function validate to check wether number of arguments is correct
 fi
 
